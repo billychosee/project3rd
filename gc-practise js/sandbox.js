930edb6c036b8f80dc857f11cad39306d31f4454
Billy <billychosee@gmail.com>
2024-04-23 11:13:00 +0200

    first commit

---
+++ gc-practise js/sandbox.js
@@ -0,0 +1,36 @@
+// const groceries =["sadza ","ne mboora"]
+// console.log(groceries)
+// function some(a,b) { return a*b;}
+// console.log(some (3,2))
+// const name =prompt("what is your name")
+// const surname =prompt("what is your surname")
+// const yob =prompt("what is your year of birth")
+
+// const sum =2024-yob
+// console.log("hi my name is " +name +" "+surname+" I am "+sum+"years old")
+// const marks =75
+// if (marks>= 75) {
+//     console.log("you have passed")
+// } else { if (marks< 74) {console.log("you have failed")
+    
+// }
+    
+//  }
+// for ( i=0; i<10; i++) {
+//    text += "number yacho " + i + "<br>";
+//  console.log (i)
+//  }
+//    { (i<10;)
+//     console.log (i);i++ 
+    
+// }
+// document.querySelector
+
+// document.getElementById('Click').addEventListener('click',function(){
+//     document.getElementById('me').innerHTML="hello guys"  
+// })
+// {console.log('button clicked')}
+
+document.getElementById("my form").addEventListener("submit",function(event) 
+{event.preventDefault();
+console.log("form submited!");});
